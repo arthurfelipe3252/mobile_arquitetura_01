2e00fba38bce7e60e1f7ce3ac17479356dd2303e
arthurfelipe3252 <arthurfelipebalmantdesouza2019@gmail.com>
2026-03-10 13:29:28 -0300

    implements system and arquiteture

---
+++ lib/data/datasources/product_remote_datasource.dart
@@ -0,0 +1,15 @@
+import 'package:mobile_arquitetura_01/core/network/http_client.dart';
+import 'package:mobile_arquitetura_01/data/models/product_model.dart';
+
+class ProductRemoteDatasource {
+  final HttpClient client;
+
+  ProductRemoteDatasource(this.client);
+
+  Future<List<ProductModel>> getProducts() async {
+    final response = await client.get("https://fakestoreapi.com/products");
+    final List data = response.data;
+    return data.map((json) => ProductModel.fromJson(json)).toList();
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_arquitetura_01/core/network/http_client.dart';
+import 'package:mobile_arquitetura_01/data/datasources/product_remote_datasource.dart';
+import 'package:mobile_arquitetura_01/data/repositories/product_repository_impl.dart';
+import 'package:mobile_arquitetura_01/presentation/pages/product_page.dart';
+import 'package:mobile_arquitetura_01/presentation/viewmodels/product_viewmodel.dart';
+
+void main() {
+  final client = HttpClient();
+  final datasource = ProductRemoteDatasource(client);
+  final repository = ProductRepositoryImpl(datasource);
+  final viewModel = ProductViewModel(repository);
+
+  runApp(MyApp(viewModel: viewModel));
+}
+
+class MyApp extends StatelessWidget {
+  final ProductViewModel viewModel;
+
+  const MyApp({super.key, required this.viewModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Product App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: ProductPage(viewModel: viewModel),
+    );
+  }
+}
